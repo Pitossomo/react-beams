e49d6341c58f6872b886462db19a6b8e3cfae7aa
pitossomo <pedro.h.teles@hotmail.com>
2022-10-03 21:33:24 -0300

    Social networks added, some style added to the page

--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -1,29 +1,36 @@
-const Controls = ({viewMode, pointerCoordinates, updateViewMode}) => (
-  <aside>
-    <div>
-      <label>Modo de visão</label>
-      <select name="view-mode" value={viewMode} className="view-mode-selector" onChange={e => updateViewMode(e.target.value)}>
-        <option value="LOADS">Cargas na Viga</option>
-        <option value="REACTIONS">Reações nos Apoios</option>
-        <option value="SHEAR">Diagrama de Esforço Contante</option>
-        <option value="MOMENTS">Diagrama de Momento Fletor</option>
-      </select>
-    </div>
-    <div className="coordinates-wrapper">
+const Controls = ({viewMode, pointerCoordinates, updateViewMode}) => {
+  const LABELS = {
+    'SHEAR': 'Esforço cortante',
+    'MOMENTS': 'Momento fletor'
+  } 
+  
+  return (
+    <aside>
       <div>
-        <strong id="x-label">x =</strong>
-        <span id="x-value">
-          { pointerCoordinates ? pointerCoordinates.x.toFixed(2).toLocaleString() : "-"}
-        </span>
+        <label>Modo de visão</label>
+        <select name="view-mode" value={viewMode} className="view-mode-selector" onChange={e => updateViewMode(e.target.value)}>
+          <option value="LOADS">Cargas na Viga</option>
+          <option value="REACTIONS">Reações nos Apoios</option>
+          <option value="SHEAR">Diagrama de Esforço Contante</option>
+          <option value="MOMENTS">Diagrama de Momento Fletor</option>
+        </select>
       </div>
-      <div>
-        <strong id="y-label">Value in x =</strong>
-        <span id="y-value">
-          { pointerCoordinates ? pointerCoordinates.y.toFixed(2).toLocaleString() : "-"}
-        </span>
+      <div className="coordinates-wrapper">
+        <div>
+          <strong id="x-label">x =</strong>
+          <span id="x-value">
+            { pointerCoordinates ? pointerCoordinates.x.toFixed(2).toLocaleString() : "-"}
+          </span>
+        </div>
+        <div>
+          <strong id="y-label">{LABELS[viewMode] || 'Valor'}=</strong>
+          <span id="y-value">
+            { pointerCoordinates ? pointerCoordinates.y.toFixed(2).toLocaleString() : "-"}
+          </span>
+        </div>
       </div>
-    </div>
-  </aside>
-)
+    </aside>
+  )
+}
 
 export default Controls
